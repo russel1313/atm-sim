7a06e81432bb1870543ae9dacfcd1812ef1327c5
russel1313 <91951449+russel1313@users.noreply.github.com>
2021-10-06 23:14:11 +0330

    A self-sufficient ATM emulator

---
+++ src/test/java/com/russel/atm/simulator/ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.russel.atm.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Rassul Hessampour
+ * @version $Revision: 1.1.0 $
+ */
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
